2694ae8993429fa974e33495416630c5e170d1b7
gayenmilan <138154602+gayenmilan@users.noreply.github.com>
2023-08-09 15:25:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AngularTest
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-2ve4gv)
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import 'zone.js/dist/zone';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppModule } from './app/app.module';
+
+
+
+bootstrapApplication(AppModule);
