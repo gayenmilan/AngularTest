e6401e2b4d5ed1816e495a25a1dfbc8da62df0f8
gayenmilan <138154602+gayenmilan@users.noreply.github.com>
2023-08-10 01:49:49 +0530

    Routing working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,17 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ClaimstatusComponent } from './claimstatus/claimstatus.component';
+import { ClaimdetailsComponent } from './claimdetails/claimdetails.component';
+
+const appRoutes: Routes = [
+  //{ path: '', component: AppComponent },
+  { path: '', component: ClaimstatusComponent },
+  { path: 'claimdetails', component: ClaimdetailsComponent },
+];
 
 @NgModule({
   imports: [
@@ -20,8 +29,9 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     MatListModule,
     MatIconModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, ClaimdetailsComponent, ClaimstatusComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {
