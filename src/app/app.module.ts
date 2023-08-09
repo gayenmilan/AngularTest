ca42ab33df085fdc184fa44ad42a933ddcbece3b
gayenmilan <138154602+gayenmilan@users.noreply.github.com>
2023-08-09 16:52:59 +0530

    Changes related the angular material add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, platformBrowser } from '@angular/platform-browser';
+import { AppMaterialModule } from './app.material.module';
+import { MatIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [ AppComponent ]
+  imports: [BrowserModule, AppMaterialModule, MatIconModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  title = 'Test Angular';
+}
+
+platformBrowser().bootstrapModule(AppModule);
