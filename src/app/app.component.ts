bfd657781704ce5ab1339ffa66faaed368de6792
gayenmilan <138154602+gayenmilan@users.noreply.github.com>
2023-08-09 21:23:43 +0530

    Completed till arrow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  userinfo = [
+    {
+      patientDetails: {
+        firstName: 'John',
+        lastName: 'Smith',
+      },
+      claimDetails: {
+        claimNumber: 'CL34246334',
+        dateSubmitted: '2007-04-26T00:00:00',
+        claimStatus: 'ADDITIONAL_INFO_REQUIRED',
+      },
+    },
+    {
+      patientDetails: {
+        firstName: 'Lara',
+        lastName: 'Smith',
+      },
+      claimDetails: {
+        claimNumber: 'CL34243337',
+        dateSubmitted: '2010-04-26T00:00:00',
+        claimStatus: 'COMPLETED',
+      },
+    },
+    {
+      patientDetails: {
+        firstName: 'Steve',
+        lastName: 'Bernard',
+      },
+      claimDetails: {
+        claimNumber: 'CL34234338',
+        dateSubmitted: '2013-04-26T00:00:00',
+        claimStatus: 'NOT_RECEIVED',
+      },
+    },
+  ];
+
+  // userinfo = [
+  //   {
+  //     patientDetails: {
+  //       firstName: 'John',
+  //       lastName: 'Smith',
+  //     },
+  //     claimDetails: {
+  //       claimNumber: 'CL34246334',
+  //       dateSubmitted: '2007-04-26T00:00:00',
+  //       claimStatus: 'ADDITIONAL_INFO_REQUIRED',
+  //     },
+  //   },
+  //   {
+  //     patientDetails: {
+  //       firstName: 'John',
+  //       lastName: 'Smith',
+  //     },
+  //     claimDetails: {
+  //       claimNumber: 'CL34243334',
+  //       dateSubmitted: '2010-04-26T00:00:00',
+  //       claimStatus: 'COMPLETED',
+  //     },
+  //   },
+  //   {
+  //     patientDetails: {
+  //       firstName: 'Steve',
+  //       lastName: 'Bernard',
+  //     },
+  //     claimDetails: {
+  //       claimNumber: 'CL34234334',
+  //       dateSubmitted: '2013-04-26T00:00:00',
+  //       claimStatus: 'NOT_RECEIVED',
+  //     },
+  //   },
+  // ];
+
   title = 'My First Project';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, platformBrowser } from '@angular/platform-browser';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    MatTabsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
